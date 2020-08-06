f500e27028a5c90b725a36066e2682f59c83ebb5
MonicaLuca <monica.lucarini28@gmail.com>
2020-08-06 15:20:10 -0400

    added secure password encryption

--- express_server.js
+++ express_server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 8080; // default port 8080
 const cookieParser = require('cookie-parser');
 const bodyParser = require("body-parser");
-const e = require("express");
+const bcrypt = require('bcrypt');
 
 app.set("view engine", "ejs");
 app.use(cookieParser());
@@ -21,30 +21,26 @@ const urlDatabase = {
   "9sm5xK": { longURL: "http://www.google.com", userID: "user2RandomID" }
 };
 
-//original database
-// "b2xVn2": "http://www.lighthouselabs.ca",
-// "9sm5xK": "http://www.google.com"
-// };
-
 
 //User Database
 const users = { 
-  "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
-    password: "purple-monkey-dinosaur"
-  },
- "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
-    password: "dishwasher-funk"
-  }
+  // "userRandomID": {
+  //   id: "userRandomID", 
+  //   email: "user@example.com", 
+  //   password: "purple-monkey-dinosaur",
+    // hashedPassword: bcrypt.hashSync(users., 10)
+  // },
+//  "user2RandomID": {
+//     id: "user2RandomID", 
+//     email: "user2@example.com", 
+//     password: "dishwasher-funk",
+//     hashedPassword: bcrypt.hashSync("dishwasher-funk", 10)
+//   }
 };
 
 // //Loops through the urlDatatbase to pull shortURL and longURL when the arg (user) matches the userID
 const findUserUrl = (user) => {
   userUrls = {}
-  console.log(user)
   for (let url in urlDatabase) {
     if (user.id == urlDatabase[url].userID) {
       userUrls[url] = urlDatabase[url] 
@@ -65,7 +61,7 @@ const findUserId = (email) => {
 //confirms if a given users email matches the password
 const checkAccountExists = (email, password) => {
   for(let user in users) {
-    if(email === users[user].email && password === users[user].password) {
+    if(email === users[user].email && bcrypt.compareSync(password, users[user].password)) {
       return true
     }
   }
@@ -96,36 +92,15 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
   
-//the urls page for a user, if the user doesn't have an account, it will redirect to login, if there is an account, it will show all urls linked to their account
-// app.get("/urls", (req, res) => {
-//   // // let userUrls = findUserUrl(users[req.cookies["user_id"]].id)
-//   // console.log(userUrls)
-//   // let templateVars = { user: users[req.cookies["user_id"]], urls: userUrls };
-//   // res.render("urls_index", templateVars);
-
-//   // let templateVars = { user: users[req.cookies["user_id"]], urls: urlDatabase };
-// //   // res.render("urls_index", templateVars);
-
-//   let userUrls = findUserUrl(users[req.cookies["user_id"]].id)
-//   let templateVars = { user: users[req.cookies["user_id"]], urls: userUrls }
-//   if (users[req.cookies["user_id"]] === undefined){
-//     res.send("error:Please login")
-//   } else {
-//     res.render("urls_index", templateVars);
-//   }
-  
-  app.get("/urls", (req, res) => {
-
+//allows users who are logged in to retrieve their saved URLS
+app.get("/urls", (req, res) => {
   if (users[req.cookies["user_id"]] === undefined){
     res.send("error:Please login")
   } else {
     let userUrls = findUserUrl(users[req.cookies["user_id"]])
     let templateVars = { user: users[req.cookies["user_id"]], urls: userUrls }
     res.render("urls_index", templateVars);
-    console.log(userUrls)
   }
-    // // let templateVars = { user: users[req.cookies["user_id"]], urls: urlDatabase };
-    // res.render("urls_index", templateVars);
 });
 
 
@@ -146,13 +121,19 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.post("/urls/:id", (req, res) => {
-  urlDatabase[req.params.id].longURL = req.body.longURL;
-  res.redirect(`/urls/${req.params.id}`);
+  console.log(users[req.cookies["user_id"]] , urlDatabase[req.params.id].userID)
+  if (users[req.cookies["user_id"]] !== urlDatabase[req.params.id].userID) {
+    res.send("error:Please login")
+  } else {
+    urlDatabase[req.params.id].longURL = req.body.longURL;
+    res.redirect(`/urls/${req.params.id}`);
+  }
+  console.log(urlDatabase[url].userID)
 });
 
-app.post("/urls/:id/edit", (req, res) => {
-  res.redirect(`/urls/${req.params.id}`);
-});
+// app.post("/urls/:id/edit", (req, res) => {
+//   res.redirect(`/urls/${req.params.id}`);
+// });
 
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { user: users[req.cookies["user_id"]], shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
@@ -160,8 +141,12 @@ app.get("/urls/:shortURL", (req, res) => {
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => {
-  delete urlDatabase[req.params.shortURL];
-  res.redirect("/urls");
+  if (users[req.cookies["user_id"]] !== urlDatabase[req.params.shortURL].userID) {
+    res.send("error:Please login")
+  } else {
+    delete urlDatabase[req.params.shortURL];
+    res.redirect("/urls");
+  }
 });
 
 app.get("/u/:shortURL", (req, res) => {
@@ -186,10 +171,9 @@ app.post("/login", (req, res) => {
   }
 });
 
-
 app.post("/logout", (req, res) => {
   res.clearCookie("user_id")
-  res.redirect("/urls")
+  res.redirect("/login")
 });
 
 app.get("/register", (req, res) => {
@@ -198,6 +182,7 @@ app.get("/register", (req, res) => {
 })
 
 app.post("/register", (req, res) => {
+  const hashedPassword = bcrypt.hashSync(req.body.password, 10)
   if(!createValidUser(req.body.email, req.body.password)){
     res.status(400).json({error:"password and email can't be blank"})
   }
@@ -208,8 +193,9 @@ app.post("/register", (req, res) => {
       users[user] = {
         id: user,
         email: req.body.email,
-        password: req.body.password
+        password: hashedPassword
       }
+      console.log(users)
       res.cookie("user_id", user);
       res.redirect("/urls")
     }
