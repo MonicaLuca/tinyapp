a634f7312c1b9529e869c01848f5ed84abfbb3a3
MonicaLuca <monica.lucarini28@gmail.com>
2020-08-06 23:31:00 -0400

    linted and comments describing functions

--- helper.js
+++ helper.js
@@ -8,31 +8,31 @@ const urlDatabase = {
 
 
 //User Database
-const users = { 
-  ecvssl: { 
+const users = {
+  ecvssl: {
     id: 'ecvssl',
     email: '123@gmail.com',
     password: bcrypt.hashSync("123", 10)
   }
 
-}
+};
 
 //generates random string used for shortURL and to generate a userID
-function generateRandomString() {
+const generateRandomString = function() {
   let id = Math.random().toString(36).slice(2,8);
   return id;
 };
 
 // //Loops through the urlDatatbase to pull shortURL and longURL when the arg (user) matches the userID
 const findUserUrl = (user) => {
-  userUrls = {}
+  userUrls = {};
   for (let url in urlDatabase) {
     if (user.id == urlDatabase[url].userID) {
-      userUrls[url] = urlDatabase[url] 
+      userUrls[url] = urlDatabase[url];
     }
   }
-  return userUrls
- };
+  return userUrls;
+};
 
 //finds userID for corresponding email
 const getUserByEmail = (email,database) => {
@@ -45,21 +45,21 @@ const getUserByEmail = (email,database) => {
 
 //confirms if a given users email matches the password
 const checkAccountExists = (email, password) => {
-  for(let user in users) {
-    if(email === users[user].email && bcrypt.compareSync(password, users[user].password)) {
-      return true
+  for (let user in users) {
+    if (email === users[user].email && bcrypt.compareSync(password, users[user].password)) {
+      return true;
     }
   }
-}
+};
  
 //checks if the submitted password or email fields are empty
 const createValidUser = (submittedEmail, submittedPassword) => {
-  if (submittedEmail === '' || submittedPassword === ''){
-    return false
+  if (submittedEmail === '' || submittedPassword === '') {
+    return false;
   } else {
-    return true
-  };
-}
+    return true;
+  }
+};
 
 //checks if the user email is already registered
 const checkDuplicateEmail = (email) => {
@@ -73,14 +73,15 @@ const checkDuplicateEmail = (email) => {
 };
 
 //checks if User is Logged In
-const userNotLoggedIn = function(req){
-  return (!req.session.user_id || req.session.user_id === undefined)
-}
-//checks if User has created url
-const userOwnsUrl = function (req){
-  const user = req.session.user_id
-  const url = urlDatabase[req.params.shortURL]
-  return (user === url.userID)
-}
+const userNotLoggedIn = function(req) {
+  return (!req.session.user_id || req.session.user_id === undefined);
+};
 
-module.exports = { generateRandomString, findUserUrl, getUserByEmail, checkAccountExists, createValidUser, checkDuplicateEmail, userOwnsUrl, userNotLoggedIn,  users, urlDatabase }
+//checks if User has created url
+const userOwnsUrl = function(req) {
+  const user = req.session.user_id;
+  const url = urlDatabase[req.params.shortURL];
+  return (user === url.userID);
+};
+
+module.exports = { generateRandomString, findUserUrl, getUserByEmail, checkAccountExists, createValidUser, checkDuplicateEmail, userOwnsUrl, userNotLoggedIn,  users, urlDatabase };
